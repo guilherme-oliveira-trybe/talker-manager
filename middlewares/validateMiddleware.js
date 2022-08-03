bd1db834f06cf033dfd8be06ae4175ec96311095
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-03 08:27:06 -0300

    feat: validador de campo Age

--- middlewares/validateMiddleware.js
+++ middlewares/validateMiddleware.js
@@ -53,4 +53,19 @@ const validateName = (req, res, next) => {
   next();
 };
 
-module.exports = { validateEmail, validatePassword, validateToken, validateName };
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  const MIN_AGE = 18;
+  if (!Number.isInteger(Number(age))) {
+    return res.status(400).end();
+  }
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < MIN_AGE) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+module.exports = { validateEmail, validatePassword, validateToken, validateName, validateAge };

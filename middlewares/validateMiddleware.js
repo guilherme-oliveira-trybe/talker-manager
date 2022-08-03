11e023d709c25eae59c7b3760a38296be638d8c9
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-03 10:33:54 -0300

    fix(validateKeyTalk): verificação de key do tipo object, alterado de lugar

--- middlewares/validateMiddleware.js
+++ middlewares/validateMiddleware.js
@@ -72,11 +72,11 @@ const validateAge = (req, res, next) => {
 
 const validateKeyTalk = (req, res, next) => {
   const { talk } = req.body;
-  if (typeof (talk) !== 'object') {
-    return res.status(400).end();
-  }
   if (!talk || talk === '') {
     return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  if (typeof (talk) !== 'object') {
+    return res.status(400).end();
   }
   next();
 };

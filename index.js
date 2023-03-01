fce23ae1f8db74a42ede4887dd960ef3b3f22852
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-01 08:10:59 -0300

    refactor: adiciona rota de erro

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const talkersRouter = require('./routers/talkerRouter');
 const loginRouter = require('./routers/loginRouter');
+require('express-rescue');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ const PORT = '3000';
 
 app.use('/talker', talkersRouter);
 app.use('/login', loginRouter);
+
+app.use((err, req, res, _next) => res.status(500).json({ message: 'Server Error!!!' }));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
